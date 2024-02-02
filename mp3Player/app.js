9f80550034c36ec02da0dd5faa5afe60c68045c9
frankdev0428@gmail.com <frankdev0428@gmail.com>
2024-02-02 09:34:46 -0800

    playSong function build

--- mp3Player/app.js
+++ mp3Player/app.js
@@ -111,6 +111,15 @@ const playSong = (id) => {
   const song = userData?.songs.find((song) => song.id === id);
   audio.src = song.src;
   audio.title = song.title;
+  if (userData?.currentSong === null || userData?.currentSong.id !== id){
+      audio.currentTime = 0 ;
+  } else {
+      audio.currentTime  = userData?.songCurrentTime;
+  }
+  userData.currentSong = song;
+  playButton.classList.add("playing");
+
+  audio.play();
 };
 
 userData?.songs.sort((a,b) => {
