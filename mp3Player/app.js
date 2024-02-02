38a709684a85b180be0d56ab8ae4039a3b20ac25
frankdev0428@gmail.com <frankdev0428@gmail.com>
2024-02-02 08:56:59 -0800

    sort playlist songs title

--- mp3Player/app.js
+++ mp3Player/app.js
@@ -117,5 +117,9 @@ userData?.songs.sort((a,b) => {
    if( a.title < b.title) {
     return -1;
    }
+   if (a.title > b.title) {
+    return 1;
+  }
+  return 0;
 });
 renderSongs(userData?.songs)
